968816d49c4471b8dd6bb4f86fac1430f7eabf4f
IvanCastroRuiz <icastror@hotmail.com>
2023-09-18 12:39:31 -0500

    data json

--- src/gestion/pages/HomePage.tsx
+++ src/gestion/pages/HomePage.tsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from "react"
 import JsonToHtml from "../components/JsonToHtml";
 import { Data } from "../interfaces/data.interface";
+import DataJSON from '../../../Data.json'
 
 const HomePage = () => {
 
   const [DataGestion, setDataGestion] = useState<Data>({ empresaEntregaPedidos: {}, sucursales: [] })
 
   useEffect(() => {
-    fetch('http://localhost:5173/data.json')
-    .then(( response ) => response.json())
-    .then(( result ) => setDataGestion( result ))
+    // fetch('http://localhost:5173/data.json')
+    // .then(( response ) => response.json())
+    // .then(( result ) => setDataGestion( result ))
+    setDataGestion( DataJSON )
   }, [])
 
   return (
